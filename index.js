2c2438bff1f1d156abc385b02cac9265185d93a8
wilf312 <g.okada.wilf+github@gmail.com>
2017-01-06 20:44:53 +0900

    update

--- index.js
+++ index.js
@@ -1,33 +1,67 @@
-import path from 'path'
-import moment from 'moment'
-import anime from 'animejs'
+/*
+ * JS utility
+ * Released under the MIT license
+ */
 
-// コンポーネントの名前を取得 ( __fileNameが渡される想定 )
-// NOTE: (要) Webpackの設定で node: { __filename: true }
-export function getFileName(fileName) {
+var path = require('path')
+var moment = require('moment')
+var anime = require('animejs')
+
+
+
+/**
+コンポーネントの名前を取得
+@param {string} fileName ファイルパス ※ __fileNameが渡される想定
+@return {string} 拡張子なしのファイル名
+@note Webpackの設定で node: { __filename: true }
+@example
+    getFileName(__fileName); //=> index
+*/
+module.exports.getFileName = function(fileName) {
     const baseName = path.basename(fileName)
     const extName = path.extname(fileName)
 
     return baseName.replace(extName, '')
 }
 
-// MySQLのdatetime形式から 2016-08-22 16:00:00 → 2016/8/22 16:00 のフォーマットで日付を返す
-export function getDateFormat(timestamp) {
+/**
+MySQLのdatetime形式を変換して日付を返す
+@param {string} timestamp ファイルパス ※ __fileNameが渡される想定
+@return {string} 'YYYY/M/D HH:mm' 形式の日付
+@note Webpackの設定で node: { __filename: true }
+@example
+    getDateFormat('2016-08-22 16:00:00'); //=> 2016/8/22 16:00
+*/
+module.exports.getDateFormat = function(timestamp) {
     const datetime = new moment(timestamp).format('YYYY/M/D HH:mm')
     return datetime
 }
 
 
-// デバッグログ
-export function log(...textList) {
+/**
+デバッグログ （本番直前でログ出てると辛いのでラッパー）
+@param {array} textList
+@example
+    log('hoge'); //=> 'hoge'
+    log('hoge', true); //=> 'hoge', true
+    log('hoge', true, {ab: 'cd'}); //=> 'hoge', true, {ab: 'cd'}
+*/
+module.exports.log = function(...textList) {
     const isDebug = true
     if (isDebug) {
         console.log.apply(console, textList)
     }
 }
 
-// $targetの位置までwindowをスクロール
-export function scrollTo($target) {
+/**
+$targetの位置までwindowをスクロール
+@param {HTMLElement} $target HTMLの要素
+@note animejs を使用している
+@example
+    scrollTo(document.querySelector('body')); //=> ページトップへスクロール
+    scrollTo(document.querySelector('#a')); //=> #a の位置までスクロール
+*/
+module.exports.scrollTo = function($target) {
   var scrollVal = 0
 
   if ($target) {
@@ -41,8 +75,6 @@ export function scrollTo($target) {
       scroll: scrollVal,
       duration: 300,
       easing: 'easeInOutQuad',
-      // loop: true,
-      // round: true,
       update: function() {
         window.scrollTo(0, animaObj.scroll)
       }
@@ -50,9 +82,17 @@ export function scrollTo($target) {
   }
 }
 
-// DOM取得
-export function $(selector) {
+/**
+HTML要素の取得
+@param {HTMLElement} selector HTMLの要素
+@example
+    $('body'); //=> body要素の取得
+    $('#a'); //=> ID a の要素を取得
 
+    // scrollToと併用すると↓のように書ける
+    scrollTo($('#a')) //=> ID a の位置へスクロール
+*/
+module.exports.$ = function(selector) {
   // Handle HTML strings
   if ( typeof selector === "string" ) {
     return document.querySelector(selector)
@@ -61,5 +101,34 @@ export function $(selector) {
   }
   else if ( selector.nodeType ) {
     return selector
+  }
+}
+
+
+
+
+
+/**
+ネイティブイベントを疑似実行する
+@param {HTMLElement} $elm HTMLの要素
+@param {string} event イベント名
+@example
+    trigger(document.querySelector('#button'), 'click')
+
+    // $と併用すると↓のように書ける
+    trigger($('#button'), 'click')
+*/
+module.exports.trigger = function($elm, event) {
+
+  if (document.createEvent) {
+    // IE以外
+    var evt = document.createEvent("HTMLEvents")
+    evt.initEvent(event, true, true )  // event type, bubbling, cancelable
+    return $elm.dispatchEvent(evt)
+  }
+  else {
+    // IE
+    var evt = document.createEventObject()
+    return $elm.fireEvent("on"+event, evt)
   }
 }
